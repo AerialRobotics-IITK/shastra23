3d09c0baf623dab14b39ca3da843987e6f1b287a
SP1029 <devang21@iitk.ac.in>
2022-12-14 23:46:20 +0530

    Pkg 2 Thresholding

--- shastra_detection/src/package_detector.cpp
+++ shastra_detection/src/package_detector.cpp
@@ -6,9 +6,7 @@
 #include<opencv2/highgui/highgui.hpp>
 #include<opencv2/aruco.hpp>
 
-static const std::string OPENCV_WINDOW = "Image window";
-
-class ImageConverter
+class Image
 {
   ros::NodeHandle nh_;
   image_transport::ImageTransport it_;
@@ -20,38 +18,29 @@ public:
   int hmin, hmax;
   int smin, smax;
   int vmin, vmax;
+  cv_bridge::CvImagePtr cv_ptr;
 
-  ImageConverter()
+  Image() 
     : it_(nh_)
   {
     // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/firefly/camera_cam/image_raw", 1,
-      &ImageConverter::imageCb, this);
-    image_pub_ = it_.advertise("/package_detector/output", 1);
+    image_sub_ = it_.subscribe("/camera/color/image_raw", 1,
+      &Image::imageCb, this);
+    image_pub_ = it_.advertise("/detection/package2", 1);
 
-    cv::namedWindow(OPENCV_WINDOW);
-
-    // // Trackbars
-    // hmin = 0; hmax = 179;
-    // smin = 0; smax = 255;
-    // vmin = 0; vmax = 255;
-    // cv::namedWindow("Trackbars",(500,500));
-    // cv::createTrackbar("H min", "Trackbars", &hmin, 179);
-    // cv::createTrackbar("H max", "Trackbars", &hmax, 179);
-    // cv::createTrackbar("S min", "Trackbars", &smin, 255);
-    // cv::createTrackbar("S max", "Trackbars", &smax, 255);
-    // cv::createTrackbar("V min", "Trackbars", &vmin, 255);
-    // cv::createTrackbar("V max", "Trackbars", &vmax, 255);
+    // Initialise Trackbars
+    hmin = 15; hmax = 30;
+    smin = 30; smax = 150;
+    vmin = 30; vmax = 85;
   }
 
-  ~ImageConverter()
+  ~Image()
   {
-    cv::destroyWindow(OPENCV_WINDOW);
+    cv::destroyAllWindows();
   }
 
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
-    cv_bridge::CvImagePtr cv_ptr;
     try
     {
       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
@@ -61,31 +50,70 @@ public:
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
-
-    // // Blurring
-    //     cv::GaussianBlur(cv_ptr->image,cv_ptr->image,cv::Size(3,3),5,5);
     
-    // // HSV
-    //     cv::cvtColor(cv_ptr->image, cv_ptr->image, cv::COLOR_BGR2HSV);
+    // Original Image Window
+    cv::imshow("Image", cv_ptr->image);
 
-    // // Threshold
-    //     cv::Scalar lower(hmin, vmin, smin);
-    //     cv::Scalar upper(hmax, vmax, smax);
-    //     cv::inRange(cv_ptr->image,lower, upper, cv_ptr->image);
+    // Create Trackbars
+    // HSV_Trackbar();
 
-    // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    // Threshold in HSV
+    Threshold();
+
+    // Thresholded Image Window
+    cv::imshow("Thresholded Image", cv_ptr->image);
     cv::waitKey(3);
 
-    // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
+    // Publish
+    Publish();
   }
+
+  void HSV_Trackbar()
+  {
+    cv::namedWindow("Trackbars",(500,500));
+
+    // Trackbars
+    cv::createTrackbar("H min", "Trackbars", &hmin, 179);
+    cv::createTrackbar("H max", "Trackbars", &hmax, 179);
+    cv::createTrackbar("S min", "Trackbars", &smin, 255);
+    cv::createTrackbar("S max", "Trackbars", &smax, 255);
+    cv::createTrackbar("V min", "Trackbars", &vmin, 255);
+    cv::createTrackbar("V max", "Trackbars", &vmax, 255);
+  }
+
+  void Threshold ()
+  {
+    // cv::Mat imgBlur;
+    cv::GaussianBlur(cv_ptr->image, cv_ptr->image, cv::Size(3,3),5,5);
+
+    // cv::Mat imgHSV;
+    cv::cvtColor(cv_ptr->image, cv_ptr->image, cv::COLOR_BGR2HSV);
+
+    cv::Scalar lower(hmin, smin, vmin);
+    cv::Scalar upper(hmax, smax, vmax);
+    cv::inRange(cv_ptr->image ,lower, upper, cv_ptr->image);
+  }
+
+  void Publish()
+  {
+    // Output modified video stream
+    cv_bridge::CvImage img_bridge;
+    sensor_msgs::Image img_msg; // message to be sent
+
+    std_msgs::Header header; // empty header
+    // header.seq = counter; // user defined counter
+    header.stamp = ros::Time::now(); // time
+    img_bridge = cv_bridge::CvImage(header,sensor_msgs::image_encodings::BGR8, cv_ptr->image); // cv to cv_bridge
+    img_bridge.toImageMsg(img_msg); // from cv_bridge to sensor_msgs::Image
+    image_pub_.publish(img_msg);
+  }
+
 };
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "package_detector");
-  ImageConverter ic;
+  Image im;
   ros::spin();
   return 0;
 }
